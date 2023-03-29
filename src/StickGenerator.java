cb4f089810b58fe318eca3c9d3681b668bf88223
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-03-28 22:18:03 -0300

    Fazendo com que a originalImage venha de uma URL da Web

--- src/StickGenerator.java
+++ src/StickGenerator.java
@@ -2,12 +2,16 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class StickGenerator {
     public void create() throws IOException {
         // Ler a imagem
-        BufferedImage originalImage = ImageIO.read(new File("images/movie.jpg"));
+        InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@.jpg").openStream();
+        BufferedImage originalImage = ImageIO.read(inputStream);
 
         // Criar nova imagem em memória com transparência e com tamanhho novo
         int width = originalImage.getWidth();
@@ -17,12 +21,12 @@ public class StickGenerator {
 
         // Copiar a imagem original para nova imagem (em memória)
         Graphics2D graphics = (Graphics2D) newImage.getGraphics();
-        graphics.drawImage(originalImage,0, 0, null);
+        graphics.drawImage(originalImage, 0, 0, null);
 
         // Setar a fonte
         var font = new Font(Font.SANS_SERIF, Font.BOLD, 52);
         graphics.setFont(font);
-        graphics.setColor(Color.YELLOW);
+        graphics.setColor(Color.WHITE);
 
         // Escrever uma frase na nova imagem
         graphics.drawString("DEIXA A ARMA, PEGUE O CANNOLI", 100, newHeight - 100);
