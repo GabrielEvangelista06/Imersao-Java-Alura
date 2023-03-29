80cbeda421fb36046a80478b4769fa4db591013c
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-03-28 21:35:15 -0300

    Criando classe StickGenerator para gerar os stickers dos filmes

---
+++ src/StickGenerator.java
@@ -0,0 +1,18 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class StickGenerator {
+    public void create() throws IOException {
+        // Ler a imagem
+
+        // Criar nova imagem em memória com transparência e com tamanhho novo
+
+        // Copiar a imagem original para nova imagem (em memória)
+
+        // Escrever uma frase na nova imagem
+
+        // Escrever a nova imagem em um arquivo
+    }
+}
