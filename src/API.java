50dba1bf722f9b245608fbb2cf99491297c58883
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-03-30 20:13:27 -0300

    Adicionando nova propriedade ao ENUM e alterando URL da API do IMDB

--- src/API.java
+++ src/API.java
@@ -1,14 +1,20 @@
 public enum API {
-    IMDB_TOP_MOVIES("https://raw.githubusercontent.com/lukadev08/lukadev08.github.io/main/apidata/imdbmostpopularmovies.json"),
-    NASA("https://api.nasa.gov/planetary/apod?api_key=6QxCgNSDuBbgIhke6m3aymdgbSIRNYndw6p84EhJ&start_date=2023-03-27&end_date=2023-03-30");
+    IMDB_TOP_MOVIES("https://raw.githubusercontent.com/lukadev08/lukadev08.github.io/main/apidata/imdbtop250moviesdata.json", new IMDBContentExtractor()),
+    NASA("https://api.nasa.gov/planetary/apod?api_key=6QxCgNSDuBbgIhke6m3aymdgbSIRNYndw6p84EhJ&start_date=2023-03-27&end_date=2023-03-30", new NasaContentExtractor());
 
     private String url;
+    private ContentExtractor extractor;
 
-    API(String url) {
+    API(String url, ContentExtractor extractor) {
         this.url = url;
+        this.extractor = extractor;
     }
 
     public String getUrl() {
         return url;
+    }
+
+    public ContentExtractor getExtractor() {
+        return extractor;
     }
 }
